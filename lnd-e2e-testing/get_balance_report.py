b4712095ef7a8692088cc000c7e6442df007bf31
Aleksandr Levchuk <alevchuk@gmail.com>
2018-07-13 20:49:17 -0700

    Update get_balance_report.py

--- lnd-e2e-testing/get_balance_report.py
+++ lnd-e2e-testing/get_balance_report.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-
+ 
 import subprocess
 import json
 import sys
-
-date = subprocess.check_output(["date", "-Iminutes"]).decode("utf-8").strip()
+ 
+date = subprocess.check_output(["date", "-Iseconds"]).decode("utf-8").strip()  # shows local Timezone
 wallet_balance = json.loads(subprocess.check_output(["lncli", "walletbalance"]).decode("utf-8"))
 channel_balance = json.loads(subprocess.check_output(["lncli", "channelbalance"]).decode("utf-8"))
 chain_txns = json.loads(subprocess.check_output(["lncli", "listchaintxns"]).decode("utf-8"))["transactions"]
-
+ 
 wallet = int(wallet_balance["confirmed_balance"])
 wallet_unconfirmed = int(wallet_balance["unconfirmed_balance"])
 channel = int(channel_balance["balance"])
 pending = int(channel_balance["pending_open_balance"])
 total = wallet + wallet_unconfirmed + pending + channel
 chain_fees = sum([int(i["total_fees"]) for i in chain_txns])
-
+ 
 if len(sys.argv) < 2 or sys.argv[1] != '--no-header':
     print(
       "Time\t\t\t"
@@ -26,7 +26,7 @@ if len(sys.argv) < 2 or sys.argv[1] != '--no-header':
       "ChainFees\t\t"
       "Total"
     )
-
+ 
 print(
   date + \
   "\t{:,}".format(wallet) + \
@@ -36,18 +36,22 @@ print(
   "\t{:,}".format(chain_fees) + \
   "\t{:,}".format(total)
 )
-
+ 
 # Setup:
 # chmod +x ~/lnd-e2e-testing/get_balance_report.py
 # ~/lnd-e2e-testing/get_balance_report.py > ~/balance_history.tab
-# corntab -e  # cron job to record balance every hour:
-# 0   *  *   *   *     ~/lnd-e2e-testing/get_balance_report.py --no-header >> ~/balance_history.tab
-
+# crontab -e
+## Text-editor will open, paste the following, save, and exit:
+## SHELL=/bin/bash
+## # m h  dom mon dow   command
+## *   *  *   *   *     (source ~/.profile; ~/lnd-e2e-testing/get_balance_report.py --no-header >> ~/balance_history.tab) 2> /tmp/stder_cron_get_balance_report
+ 
 # Check balance:
-# while :; do (cat ~/balance_history.tab; ~/lnd-e2e-testing/get_balance_report.py ) | column -t; date; sleep 60; done
+# while :; do (cat ~/balance_history.tab; ~/lnd-e2e-testing/get_balance_report.py) | column -t; sleep 60; done
+ 
 #
 # Example Output:
-# Time                   Wallet       Unconfirmed  Pending    Channel      ChainFees  Total
-# 2018-07-12T20:18-0700  154,430,415  0            0          124,546,659  71,093     278,977,074
-# 2018-07-13T07:36-0700  153,211,432  0            0          123,710,571  74,055     276,922,003
-# 2018-07-13T07:58-0700  144,057,139  9,152,804    3,221,709  119,914,308  75,177     273,124,251
+# Time                      Wallet       Unconfirmed  Pending    Channel      ChainFees  Total
+# 2018-07-12T20:18:00-0700  154,430,415  0            0          124,546,659  71,093     278,977,074
+# 2018-07-13T07:36:00-0700  153,211,432  0            0          123,710,571  74,055     276,922,003
+# 2018-07-13T07:58:00-0700  144,057,139  9,152,804    3,221,709  119,914,308  75,177     273,124,251
